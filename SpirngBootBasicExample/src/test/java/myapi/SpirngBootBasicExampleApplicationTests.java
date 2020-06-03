638daf4f57ebe3afcff45df57484c00dd4927fbd
Ankit Narang <ankit.5389@gmail.com>
2020-06-03 12:43:49 +0530

    Spring Boot Basic Example

---
+++ SpirngBootBasicExample/src/test/java/myapi/SpirngBootBasicExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package myapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngBootBasicExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
